75607dbb5a831d0ff14db05c09e5c440974c691c
welisgithub <welissonfilipe@gmail.com>
2026-06-12 07:35:28 -0300

    3th commit - Criação da classe Filme

---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+    String nome;
+    String genero;
+    int anoDeLancamento;
+    double notaMedia;
+    int numeroDeAvaliacoes;
+    Boolean incluidoNoPlano;
+    int duracaoEmMinutos;
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Filme panicoDois = new Filme();
+        panicoDois.nome = "Pânico 2";
+        panicoDois.anoDeLancamento = 1997;
+        panicoDois.genero = "Terror";
+        panicoDois.incluidoNoPlano = true;
+        panicoDois.notaMedia = 6.4;
+        panicoDois.duracaoEmMinutos = 120;
+        panicoDois.numeroDeAvaliacoes = 100000;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println(panicoDois.nome);
+        System.out.println(panicoDois.anoDeLancamento);
+        System.out.println(panicoDois.genero);
+        System.out.println(panicoDois.incluidoNoPlano);
+        System.out.println(panicoDois.notaMedia);
+        System.out.println(panicoDois.duracaoEmMinutos);
+        System.out.println(panicoDois.numeroDeAvaliacoes);
     }
 }
