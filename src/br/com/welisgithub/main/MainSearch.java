4427a881d777d064c67ebf1e9b754a019170eee3
welisgithub <welissonfilipe@gmail.com>
2026-06-18 22:04:41 -0300

    25th commit - API

---
+++ src/br/com/welisgithub/main/MainSearch.java
@@ -0,0 +1,19 @@
+package br.com.welisgithub.main;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MainSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com/?i=tt3896198&apikey=bb9f170a"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+    }
+}
